4cc1088b50e703b522318248c33030e9f28379cc
nigelroi <136414069+nnog23@users.noreply.github.com>
2024-11-22 22:39:44 +0800

    Delete backend directory

--- backend/models/Progress.js
+++
@@ -1,6 +0,0 @@
-const ProgressSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    decksStudied: [{ deckId: mongoose.Schema.Types.ObjectId, dateStudied: Date, score: Number }],
-}, { timestamps: true });
-
-module.exports = mongoose.model('Progress', ProgressSchema);
