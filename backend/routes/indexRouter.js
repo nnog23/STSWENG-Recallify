fe579249935660c11b15342c77df48b95e942706
nigelroi <nigel_nograles@dlsu.edu.ph>
2024-11-25 22:35:36 +0800

    Update indexRouter.js

--- backend/routes/indexRouter.js
+++ backend/routes/indexRouter.js
@@ -3,23 +3,15 @@ import cardsRouter from './cardsRouter.js';
 
 const router = Router();
 
-
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-
-
 router.get("/home", (req, res) => {
     res.redirect("/");
-}); 
+});
 
 router.get("/homepage", (req, res) => {
     res.redirect("/");
 });
 
-
 router.use(cardsRouter);
 
 
-// Export the router
-export default router;;
+export default router;
