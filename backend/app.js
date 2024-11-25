ef91b1329a5a5580df76cea82999d35beaa28ec5
nigelroi <nigel_nograles@dlsu.edu.ph>
2024-11-26 00:17:37 +0800

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -8,13 +8,14 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+    res.send('Server is running!');
+});
+
 app.use((req, res) => {
     res.status(404).send('404: Route not found.');
 });
 
-app.get('/', (req, res) => {
-    res.send('Server is running!');
-});
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
