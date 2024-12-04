54083b9a112c8f0d518868cde8dd7ed68f363f61
nigelroi <nigel_nograles@dlsu.edu.ph>
2024-12-04 09:33:14 +0800

    hotfix/app.js

--- backend/app.js
+++ backend/app.js
@@ -20,12 +20,16 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+app.options('*', cors(corsOptions));
+
+/*
 app.use((req, res, next) => {
    // res.header('Access-Control-Allow-Origin', '*');  // Allow all origins
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');  // Allow these HTTP methods
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');  // Allow these headers
     next();
   });
+*/
 
 /*
 app.use(cors({
